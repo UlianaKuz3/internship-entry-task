15db30a32a1a467d5185ad9b7bd86d4d1e188896
Uliana333 <kua.21@uni-dubna.ru>
2025-07-19 11:22:12 +0300

    Добавлена миграция

---
+++ TicTacToe.Api/Migrations/20250719082020_AddGameApi.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacToe.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameApi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MoveCount",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Symbol",
+                table: "Moves",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(char),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Player",
+                table: "Moves",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CurrentPlayer",
+                table: "Games",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(char),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Player",
+                table: "Moves");
+
+            migrationBuilder.AlterColumn<char>(
+                name: "Symbol",
+                table: "Moves",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<char>(
+                name: "CurrentPlayer",
+                table: "Games",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MoveCount",
+                table: "Games",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
